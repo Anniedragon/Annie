e79ae5e7232038ce876ebc0b17cad572c2763002
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2019-09-27 12:28:13 +0300

    Update Lab8.py

--- Lab8.py
+++ Lab8.py
@@ -11,3 +11,14 @@ else :
   A = 0
   B = 0
   print("Новые значения А и В",A,B)
+
+#lab 8, task 2
+A = int(input("Введите число A "))
+B = int(input("Введите число B "))
+C = int(input("Введите число С "))
+if (A < B < C) or (A < C < B) :
+  print(B + C)
+elif (C < B < A) or (C < A < B) : 
+  print(A + B)
+elif (B < A < C) or (B < C < A) :
+  print(A + C)
