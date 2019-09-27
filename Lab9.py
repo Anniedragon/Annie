bf907365cd50e07f039241824efe4c75c9cef36e
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2019-09-27 19:20:47 +0300

    Update Lab9.py

--- Lab9.py
+++ Lab9.py
@@ -7,147 +7,22 @@ s1 = Chis[Ch - 1] + " " + Mes[M - 1]
 print(s1)
 
 #lab 9, task 5
+col = ["зелёной", "красной", "жёлтой", "белой", "чёрной"]
+col1 =["зелёного", "красного", "жёлтого", "белого", "чёрного"]
+anim = ["крысы", "коровы", "тигра", "зайца", "дракона", "змеи", "лошади", "овцы", "обезьяны", "курицы", "собаки", "свиньи"]
 year = int(input("Введите номер года "))
-C = abs(1984 - year) #считаем остаток 
-while C > 60 : # проверяем на полный цикл
+C = abs(1984 - year)
+while C > 60 :
  C = C - 60
-k = int(C / 12) # считаем целое колисество подциклов с округлением в меньшую сторону
-PodC = C - k * 12 # считаем, сколько дней осталось, после полных подциклов
+k = int(C / 12)
+PodC = C - k * 12
 k1 = C / 12 
 if k != k1 :
  ch = k + 1 
 else :
   ch = k
-if ch == 1 :
-  s = " год зелёной "
-  if PodC == 1 :
-    s = s + "крысы"
-  elif PodC == 2 :
-    s = s + "коровы"
-  elif PodC == 3 :
-    s = s[:10] + "ого " + "тигра"
-  elif PodC == 4 :
-    s = s[:10] + "ого " +  "зайца"
-  elif PodC == 5 :
-    s = s[:10] + "ого " + "дракона"
-  elif PodC == 6 :
-    s = s + "змеи"
-  elif PodC == 7 :
-    s = s + "лошади"
-  elif PodC == 8 :
-    s = s + "овцы"
-  elif PodC == 9 :
-    s = s + "обезьяны"
-  elif PodC == 10:
-    s = s + "курицы"
-  elif PodC == 11:
-    s = s + "собаки"
-  elif PodC == 12:
-    s = s + "свиньи"
-if ch == 2 :
-  s = " год красной "
-  if PodC == 1 :
-    s = s + "крысы"
-  elif PodC == 2 :
-    s = s + "коровы"
-  elif PodC == 3 :
-    s = s[:10] + "ого " + "тигра"
-  elif PodC == 4 :
-    s = s[:10] + "ого " + "зайца"
-  elif PodC == 5 :
-    s = s[:10] + "ого " + "дракона"
-  elif PodC == 6 :
-    s = s + "змеи"
-  elif PodC == 7 :
-    s = s + "лошади"
-  elif PodC == 8 :
-    s = s + "овцы"
-  elif PodC == 9 :
-    s = s + "обезьяны"
-  elif PodC == 10:
-    s = s + "курицы"
-  elif PodC == 11:
-    s = s + "собаки"
-  elif PodC == 12:
-    s = s + "свиньи"
-if ch == 3 :
-  s = " год жёлтой "
-  if PodC == 1 :
-    s = s + "крысы"
-  elif PodC == 2 :
-    s = s + "коровы"
-  elif PodC == 3 :
-    s = s[:10] + "ого " + "тигра"
-  elif PodC == 4 :
-    s = s[:10] + "ого " + "зайца"
-  elif PodC == 5 :
-    s = s[:10] + "ого " + "дракона"
-  elif PodC == 6 :
-    s = s + "змеи"
-  elif PodC == 7 :
-    s = s + "лошади"
-  elif PodC == 8 :
-    s = s + "овцы"
-  elif PodC == 9 :
-    s = s + "обезьяны"
-  elif PodC == 10:
-    s = s + "курицы"
-  elif PodC == 11:
-    s = s + "собаки"
-  elif PodC == 12:
-    s = s + "свиньи"
-if ch == 4 :
-  s = " год белой "
-  if PodC == 1 :
-    s = s + "крысы"
-  elif PodC == 2 :
-    s = s + "коровы"
-  elif PodC == 3 :
-    s = s[:10] + "ого " + "тигра"
-  elif PodC == 4 :
-    s = s[:10] + "ого " + "зайца"
-  elif PodC == 5 :
-    s = s[:10] + "ого " + "дракона"
-  elif PodC == 6 :
-    s = s + "змеи"
-  elif PodC == 7 :
-    s = s + "лошади"
-  elif PodC == 8 :
-    s = s + "овцы"
-  elif PodC == 9 :
-    s = s + "обезьяны"
-  elif PodC == 10:
-    s = s + "курицы"
-  elif PodC == 11:
-    s = s + "собаки"
-  elif PodC == 12:
-    s = s + "свиньи"
-if ch == 5 :
-  s = " год чёрной "
-  if PodC == 1 :
-    s = s + "крысы"
-  elif PodC == 2 :
-    s = s + "коровы"
-  elif PodC == 3 :
-    s = s[:10] + "ого " + "тигра"
-  elif PodC == 4 :
-    s = s[:10] + "ого " + "зайца"
-  elif PodC == 5 :
-    s = s[:10] + "ого " + "дракона"
-  elif PodC == 6 :
-    s = s + "змеи"
-  elif PodC == 7 :
-    s = s + "лошади"
-  elif PodC == 8 :
-    s = s + "овцы"
-  elif PodC == 9 :
-    s = s + "обезьяны"
-  elif PodC == 10:
-    s = s + "курицы"
-  elif PodC == 11:
-    s = s + "собаки"
-  elif PodC == 12:
-    s = s + "свиньи"
-Year = str(year) + " -" + s
-print(Year)
-
+if (PodC != 3) and (PodC != 4) and (PodC != 5) :
+ s = str(year) + " - " + col[ch - 1] + " " + anim[PodC - 1]
+else :
+ s = str(year) + " - " + col1[ch - 1] + " " + anim[PodC - 1]
+print(s)
