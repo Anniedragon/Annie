87891a450f7d738f5812ae156ff0cbaa465f8157
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2019-09-30 13:25:39 +0300

    Update Lab10.py

--- Lab10.py
+++ Lab10.py
@@ -3,3 +3,10 @@ Stoim = float(input("Введите стоимость 1 кг конфет "))
 N = 10
 for i in range(1, N) :
   print("Стоимость",(i/10),"кг конфет равна:",round((Stoim*(i/10)),3))
+  
+#lab 10, task 2
+N = int(input("Введите число "))
+pr = 1
+for i in range(1, N+1) :
+ pr = pr * (1 + (i/10))
+print(pr)
