4e0aa8216ad62d169adb1b7577df96cb57e65e65
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2019-10-03 14:19:00 +0300

    Create Lab11.py

---
+++ Lab11.py
@@ -0,0 +1,9 @@
+#lab 11, task 1
+A = int(input("Введите число А "))
+B = int(input("Введите число В, большее, чем А "))
+while (A > B) or (A == B) :
+  print("Условие не выполнено!")
+  B = int(input("Введите число В, большее, чем А "))
+for i in range(A, B+1) :
+  for j in range(1, i+1) :
+    print(i)
