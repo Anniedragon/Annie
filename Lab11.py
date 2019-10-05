33352d454adb5ff5ef92a61027fb74e136f47a4d
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2019-10-05 16:34:33 +0300

    Update Lab11.py

--- Lab11.py
+++ Lab11.py
@@ -25,3 +25,14 @@ if B == k * A :
 else :
   Ost = B - (k - 1) * A
 print(Ost)
+
+#lab 11, task 3
+N = int(input("Введите число, большее 1 "))
+while N <= 1 :
+  N = int(input("Введите число, большее 1 "))
+K = 0
+S = 0
+while S <= N :
+  S += K
+  K += 1
+print(K,",","Сумма равна:",S)
