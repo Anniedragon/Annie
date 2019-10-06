6d5187646c819c3dbd2e174153a8a93febb85074
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2019-10-06 21:43:08 +0300

    Update Lab12.py

--- Lab12.py
+++ Lab12.py
@@ -22,6 +22,18 @@ def Sign(X) :
 X = float(input("Введите число "))
 Sign(X)
 
+#lab 12, task 3 Почему вывод функции крашится из-за "проверки на дурака"?
+def RingS(R1, R2) :
+  print(abs(round((3.14 * R1**2) - (3.14 * R2**2),3)))
+for i in range(1, 4) :
+ Rad1 = float(input("Введите 1ый радиус "))
+ Rad2 = float(input("Введите 2ой радиуc "))
+ #while (Rad1 < Rad2):
+  #print("1ый радиус должен быть больше 2ого!")
+  #Rad2 = float(input("Введите 2ой радиус ")
+ RingS(Rad1, Rad2) 
+ 
+
 
 
 
