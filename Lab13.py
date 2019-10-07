18f320927672246aa524ae70adb04eb58e4f174a
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2019-10-07 13:09:32 +0300

    Update Lab13.py

--- Lab13.py
+++ Lab13.py
@@ -1,5 +1,8 @@
 #lab 13, task 1
-N = int(input("Введите число "))
+N = int(input("Введите число, большее 0 "))
+while N == 0 :
+  print("Число не должно быть равно 0")
+  N = int(input("Введите число, большее 0 "))
 M = []
 for i in range(1, 2*N+1, 2) :
   M.append(i)
