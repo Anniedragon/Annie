7e061e6c84a43a78b96f36a251b6f97f53377cbb
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2019-10-10 12:06:33 +0300

    Update Lab14.py

--- Lab14.py
+++ Lab14.py
@@ -10,3 +10,22 @@ for i in range(1, N+1) :
 for i in range(K, L+1) :
   Sum += A[i]
 print("Среднее арифметическое элементов с номерами от K до L:",Sum / ((L+1)-K+1))
+
+#lab 14, task 2 In progress
+A = []
+N = int(input("Введите к-во элементов списка "))
+k = 0
+for i in range(1, N+1) :
+  A.append(int(input("Введите элемент списка (одинаковых чисел быть не должно) ")))
+while True :
+  for i in A :
+    for j in A :
+      if i == j :
+        k += 1
+  if k > N :
+    print("Условие не выполнено ")
+    for i in range(1, N+1) :
+      A.append(int(input("Введите элемент списка (одинаковых чисел быть не должно) ")))
+  else :
+    break   
+print(A)
