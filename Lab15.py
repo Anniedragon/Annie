ddb8324f8b4d270fa2ca9c84831e960548c2b567
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2019-10-12 19:32:26 +0300

    Update Lab15.py

--- Lab15.py
+++ Lab15.py
@@ -12,3 +12,17 @@ for i in range(N) :
   B[i] = temp
 print(A)
 print(B)
+
+#lab 15, task 2 In progress
+A = []
+B = []
+sum = 0
+k = 1
+N = int(input("Введите к-во элементов списка "))
+for i in range(N) :
+  A.append(int(input("Введите элемент списка А ")))
+for i in range(N) :
+  for k in range(1, i+1) :
+    sum += k
+  B[i] = sum / (k)
+print(B)
