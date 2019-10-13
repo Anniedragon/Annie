f33ce3526bf42c606f63f5b623230d350a45b83f
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2019-10-13 19:33:49 +0300

    Update Lab16.py

--- Lab16.py
+++ Lab16.py
@@ -16,3 +16,30 @@ if flag == 1 :
   print("В списке нет одинаковых элементов")
 else :
   print(A)
+
+#lab 16, task 2 In progress
+A = []
+k = 0
+N = int(input("Введите к-во элементов списка "))
+for i in range(N) :
+  A.append(int(input("Введите элемент списка ")))
+while True :
+  for i in range(N-3) :
+    for j in range(N-3) :
+      if (N <= 2) :
+        break
+      if (A[i] == A[j]) and (i != j) :
+        print(N)
+        k += 1
+        ni = i
+        nj = j
+        print(A)
+        if k == 2 :
+          k = 0
+          A.remove(A[ni])
+          A.remove(A[nj])
+          N = N - 2
+          print(A)
+        elif k == 0 :
+          break
+print(A)
