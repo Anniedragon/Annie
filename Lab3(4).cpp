ea89d035fe80fb0987f22f7717e45419663c03de
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2019-10-13 15:51:18 +0300

    Update Lab3(4).cpp

--- Lab3(4).cpp
+++ Lab3(4).cpp
@@ -25,3 +25,37 @@ int main() {
   cout << "Произведение нечётных чисел равно:" << pr << endl;
   cout << "Количество нечётных чисел равно:" << count << endl;
   }
+
+//lab 3, task 2
+#include <iostream>
+using namespace std;
+int main(){
+  int x = 0;
+  int max;
+  int min;
+  cout << "Input number of elements ";
+  cin >> x;
+  int temp[x];
+  for(int k = 1; k<=x; k++){
+    cout << "Input " << k << " element ";
+    cin >> temp[k];
+  }
+  for (int i=0; i<x; i++){
+    if(i == 0){
+      min = temp[i];
+    }
+    if(temp[i]<min){
+      min = temp[i];
+    }
+  } 
+  for (int j=0; j<x; j++){
+    if(j == 0){
+      max = temp[j];
+    }
+    if(temp[j]>max){
+      max = temp[j];
+    }
+  } 
+  cout << "Min element is " << min << endl;
+  cout << "Max element is " << max << endl;
+}
