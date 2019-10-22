09d9e4abb938e7ec608390aba0ffb5f8beac9193
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2019-10-22 15:40:38 +0300

    Update Lab4(5).cpp

--- Lab4(5).cpp
+++ Lab4(5).cpp
@@ -33,3 +33,49 @@ int main() {
   }
   cout << "Minimum = " << min << endl;
 }
+
+//lab 4(5), task 2 In progress
+#include <iostream>
+using namespace std;
+int main() {
+  int N;
+  int k = 0;
+  int temp = 0;
+  int maxi = 0;
+  int maxj = 0;
+  cout << "Enter N ";
+    cin >> N;
+  int A[N][N];
+  for (int i=1; i<=N; i++) {
+    for (int j=1; j<=N; j++) {
+        cout << "Enter element ";
+        cin >> A[i][j];
+      }
+    }
+  int max = A[0][0];
+  for (int i=0; i<=N; i++) {
+    for (int j=0; j<=N; j++) {
+      if (j <= N) {
+        if (A[i][j] > max) {
+          max = A[i][j];
+          maxi = i;
+          maxj = j;
+        }
+        cout << max << endl;
+      }
+      if (i == j) {
+        temp = A[i][j];
+        A[i][j] = A[maxi][maxj];
+        A[maxi][maxj] = temp;
+      }
+    }
+  }
+  for (int i=0; i<=N; i++) {
+    for (int j=0; j<=N; j++) {
+      if (j == N) {
+        cout << A[i][j] << endl;
+      }
+      else cout << A[i][j] << " ";
+    }
+  }
+}
