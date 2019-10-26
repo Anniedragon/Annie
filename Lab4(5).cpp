f47a23ac68c540c26261eb30ae4cb59e99133a91
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2019-10-26 16:46:33 +0300

    Update Lab4(5).cpp

--- Lab4(5).cpp
+++ Lab4(5).cpp
@@ -145,3 +145,46 @@ int main() {
     cout << k << " ";
   }
 }
+
+//lab4(5), task 5
+#include <iostream>
+using namespace std;
+int main() {
+  cout << "Cin razmer matiz ";
+  int x, y, f=0, n=0;
+  int sum1=0, sum2=0, ymn1=1, ymn2=1, x1=0, x2=0, x3=0, x4=0;
+  cin >> x;
+  y = x;
+  int temp[x][y];
+  for(int i = 0; i<x; i++){
+    for(int j = 0; j<y; j++){
+      cin >> temp[i][j];
+    }
+  }
+  for(int i = 0; i<x; i++){
+    for(int j = 0; j<y; j++){
+     if((i == f)&&(j>n)){
+       sum1 = temp[i][j] + sum1;
+       if(temp[i][j]<0){
+         sum2 = sum2 + temp[i][j];
+         x2++;
+       }
+       x1++;
+     }
+     if((i == f)&&(j<n)){
+       ymn1 = temp[i][j] * ymn1;
+       if(temp[i][j]>0){
+         ymn2 = ymn2 * temp[i][j];
+         x4++;
+       }
+       x3++;
+     }  
+    }
+    n++;
+    f++;
+  }
+  cout << "1. " << ymn1 << " " << x3 << endl;
+  cout << "2. " << sum1 << " " << x1 << endl;
+  cout << "3. " << ymn2 << " " << x4 << endl;
+  cout << "4. " << sum2 << " " << x2 << endl;
+}
