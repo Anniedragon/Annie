ec7b553b5177330532424d13a1a8a2a557332f28
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2019-10-27 18:17:23 +0300

    Update Lab5(7).cpp

--- Lab5(7).cpp
+++ Lab5(7).cpp
@@ -44,3 +44,28 @@ int main() {
   else std:: cout << "Строки не равны";
 }
 //lab5(7), task 4
+#include <iostream>
+#include <cctype>
+#include <string>
+#include <algorithm>
+int main() {
+  setlocale(LC_ALL, "Russian");
+  std::cout << "Введите первую строку ";
+  std::string temp;
+  std::string temp2;
+  std::cin >> temp;
+  transform(temp.begin(), temp.end(), temp.begin(), tolower);
+  for(int i = 0;i<temp.length(); i++){
+    if(temp[i] == ' '){
+      temp.erase(i,1);
+      i--;
+    }
+  }
+  temp2 = temp;
+  std::reverse(temp2.begin(), temp2.end());
+  if(temp == temp2){
+    std:: cout << "Строка является переветрышем";
+  }
+  else std:: cout << "Строка не является перевертышем";
+  std:: cout << temp2;
+}
