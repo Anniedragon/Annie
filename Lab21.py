9210b7343e46e29b8a0d0df845989dbc0bc92b3d
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2019-11-06 19:52:51 +0300

    Update Lab21.py

--- Lab21.py
+++ Lab21.py
@@ -24,3 +24,16 @@ for i in range(len(A)) :
   if (A[i] < Min) and (A[i] != 0) :
     Min = A[i]
 print("Длина самого короткого слова в строке равна",Min)
+
+#lab 21, task 3
+S = str(input("Введите строку из нескольких слов "))
+S1 = ""
+S = S.upper()
+for i in range(len(S)) :
+  if (S[i] == S[0]) and (i == 0):
+    S1 += S[i]
+  elif (S[i] == S[0]) and (i != 0) :
+    S1 += "."
+  else :
+    S1 += S[i] 
+print(S1)
