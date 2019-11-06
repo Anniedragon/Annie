2005c16f2f8525bcc643f9d2488249984cbaad91
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2019-11-06 19:42:02 +0300

    Update Lab21.py

--- Lab21.py
+++ Lab21.py
@@ -7,3 +7,20 @@ for i in range(len(S)) :
   if S[i] == " " :
     k += 1
 print("Количество слов в строке равно",k)
+
+#lab 21, task 2
+S = str(input("Введите строку из нескольких слов "))
+S += " "
+A = []
+k = 0
+for i in range(len(S)) :
+  if S[i] != " " :
+    k += 1
+  else :
+    A.append(k)
+    k = 0
+Min = A[0]
+for i in range(len(A)) :
+  if (A[i] < Min) and (A[i] != 0) :
+    Min = A[i]
+print("Длина самого короткого слова в строке равна",Min)
