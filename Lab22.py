9c181451112461a84ada5fbf513f6272a62e0f32
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2019-11-08 17:02:19 +0300

    Update Lab22.py

--- Lab22.py
+++ Lab22.py
@@ -5,3 +5,10 @@ for i in range(len(S)) :
     S = S[i+1:]
     break
 print(S)
+
+#lab 22, task 2
+S = "*"
+K = int(input("Введите к-во * в строке "))
+N = int(input("Введите к-во строк "))
+for i in range(N) :
+  print(S * K, end = "\n")
