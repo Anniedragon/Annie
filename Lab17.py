aa035ee2bd8e87d384016ef7ac08d6fe37e8ebeb
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2019-11-20 17:35:54 +0300

    Update Lab17.py

--- Lab17.py
+++ Lab17.py
@@ -1,28 +1,20 @@
-#lab 17, task 1 Способ 1
+#lab 17, task 1
 A = []
 B = []
 C = []
 k = 1
-flag = 1
-N = int(input("Введите к-во переменных "))
+N = int(input("Введите к-во элементов "))
 for i in range(N) :
   A.append(int(input("Введите элемент ")))
-while flag == 1 :
-  flag = 0
-  for i in range(N-1) :
-    if A[i] == A[i+1] :
-      k += 1
-      print(k)
-    else :
-      print(A)
-      print("k=",k)
-      B.append(k)
-      C.append(A[i])
-      k = 1
-      for j in range(k) :
-        A.remove(A[i])
-        N -= 1
-      flag = 1
-      break
+A.append(0)
+N += 1
+for i in range(N-1) :
+  if A[i] == A[i+1] :
+    k += 1
+  else :
+    B.append(A[i])
+    C.append(k)
+    k = 1
 print(B)
 print(C)
+
