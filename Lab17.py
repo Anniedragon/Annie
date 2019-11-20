d8d1b4e93892dee5e8989dcc26698811b36386f5
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2019-11-20 17:44:34 +0300

    Update Lab17.py

--- Lab17.py
+++ Lab17.py
@@ -18,3 +18,26 @@ for i in range(N-1) :
 print(B)
 print(C)
 
+#lab 17, task 2
+A = []
+A1 = []
+k = 1
+N = int(input("Введите к-во элементов "))
+for i in range(N) :
+  A.append(int(input("Введите элемент ")))
+L = int(input("Введите длину серии "))
+A.append(0)
+N += 1
+for i in range(N-1) :
+  if A[i] == A[i+1] :
+    k += 1
+  else :
+    if k == 1 :
+      A1.append(A[i])
+    elif k > L :
+      A1.append(0)
+      k = 1
+    else :
+      A1.append(A[i])
+      k = 1
+print(A1)
