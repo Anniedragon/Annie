503cbb55f998e2c4b1a341e2f5a7655b9df1f2fa
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2019-11-29 16:42:20 +0300

    Update Lab19.py

--- Lab19.py
+++ Lab19.py
@@ -41,3 +41,44 @@ for i in range(N) :
       print(A[i][j],"\t",end = "")
     else :
       print(A[i][j])
+
+#lab 19, task 2
+from random import randint
+N = int(input("Введите количество строк "))
+M = int(input("Введите к-во элементов в строке "))
+Min = 100
+Max = -100
+NumMn = 0
+NumMx = 0
+temp = 0
+A = []
+for i in range(N) :
+  A.append([])
+  for j in range(M) :
+    A[i].append(randint(1, 10))
+for i in range(N) :
+  for j in range(M) :
+    if j != M-1 :
+      print(A[i][j],"\t",end = "")
+    else :
+      print(A[i][j])
+print("\n")
+for i in range(N) :
+  for j in range(M) :
+    if A[i][j] < Min :
+      Min = A[i][j]
+      NumMn = j
+    elif A[i][j] > Max :
+      Max = A[i][j]
+      NumMx = j
+for i in range(N) :
+  for j in range(M) :
+    temp = A[i][NumMn]
+    A[i][NumMn] = A[i][NumMx]
+    A[i][NumMx] = temp
+for i in range(N) :
+  for j in range(M) :
+    if j != M-1 :
+      print(A[i][j],"\t",end = "")
+    else :
+      print(A[i][j])
