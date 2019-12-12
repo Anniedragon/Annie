e8cbe2710e79fd24f7d2cb33dcecb051db6fb8ad
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2019-12-12 15:25:54 +0300

    Update Lab22.py

--- Lab22.py
+++ Lab22.py
@@ -18,3 +18,23 @@ with open(path, "w") as file :
     file.write("".join(textfile))
 with open(path, "r") as file :
     print(file.read())
+
+#lab 22, task 2
+#C:\Users\agush\Desktop\Мосполитех. Программирование\example.txt
+print("Enter the full name(path) of the file ")
+path = str(input())
+N = int(input("Введите к-во строк "))
+K = int(input("Введите к-во * в строке "))
+textfile = []
+element = ""
+line = ""
+for i in range(K) :
+    line += "*"
+file = open(path, "w")
+for i in range(N) :
+    file.write(line + "\n")
+file.close()
+with open(path, "r") as file :
+    print(file.read())
+    
+
